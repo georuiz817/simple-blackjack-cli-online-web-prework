5d5b4ec56285e66a922fdd2e321470d0e2de24e3
George Ruiz <georuiz817@gmail.com>
2019-05-20 17:42:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,9 +57,9 @@ end
   # code runner here
   welcome  
   card_total = initial_round
-  until card_total < 21
     card_total = hit?(card_total)
     card_total = display_card_total(card_total)
+    until card_total < 21
   end
   end_game(card_total)
 
